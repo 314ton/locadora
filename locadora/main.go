e38805efd2a97aa134962da7f500323b60a8e424
Etho Domingues <ethodomingues3@gmail.com>
2020-08-31 19:10:21 -0300

    Canjica

--- locadora/main.go
+++ locadora/main.go
@@ -2,6 +2,9 @@ package main
 
 import (
 	"fmt"
+	"os"
+	"os/exec"
+	"runtime"
 )
 
 
@@ -12,12 +15,8 @@ type filme struct{
 }
 
 var carrinho []filme
-
-// certo, porem redundante
-//var lista []filme = []filme{} 
-
 // certo e simplificado
-var lista []filme{
+var lista = []filme{
 	filme{
 		nome: "shazam",
 		preço: 22.50,
@@ -29,9 +28,48 @@ var lista []filme{
 		desc: "após o início da pandemia, ninguém saiu de casa",
 	},
 	filme{
+		nome: "homem Aranha 2",
+		preço: 30.00,
+		desc: "após o início da pandemia, ninguém saiu de casa",
+	},
+	filme{
+		nome: "homem Aranha 3",
+		preço: 30.00,
+		desc: "após o início da pandemia, ninguém saiu de casa",
+	},
+	filme{
 		nome: "homem de ferro",
 		preço: 21.00,
 		desc: "'Eu sou o homem de ferro', e morreu",
+	},
+	filme{
+		nome: "homem de ferro 2",
+		preço: 22.00,
+		desc: "'Eu sou o homem de ferro', e morreu",
+	},
+	filme{
+		nome: "homem de ferro 3",
+		preço: 23.00,
+		desc: "'Eu sou o homem de ferro', e morreu",
+	},
+}
+
+
+var cmd = map[string]func(){
+	"linux": func(){
+		comm := exec.Command("clear")
+		comm.Stdout = os.Stdout
+		comm.Run()
+	},
+	"android": func(){
+		comm := exec.Command("clear")
+		comm.Stdout = os.Stdout
+		comm.Run()
+	},
+	"windows": func(){
+		comm := exec.Command("cls")
+		comm.Stdout = os.Stdout
+		comm.Run()
 	},
 }
 
@@ -43,7 +81,13 @@ func main(){
 	}
 	for{
 		// limpar a tela
-		print("\033[H\033[2J")
+		
+		clear, err := cmd[runtime.GOOS]
+		if err {
+			clear()
+		}
+		//print("\033[H\033[2J")
+		
 		if erros["ja_é_meu"] {
 			fmt.Println("\033[91mEsse Item já é seu!\033[m")
 			erros["ja_é_meu"] = false
@@ -58,22 +102,36 @@ func main(){
 				}
 			}
 			if vigia{
-				fmt.Println("\033[34m[",ind,"]",val.nome,"\033[m")
-				fmt.Printf("->\033[94m\tpreço: R$%.2f\033[32m COMPRADO✓\033[m\t\n",val.preço)
+				fmt.Printf(
+					"\033[34m[ %d ] %s \033[m\n",
+					ind, val.nome,
+				)
+				
+				fmt.Printf(
+					"preço:\033[94m R$%.2f\033[32m COMPRADO✓\033[m\n",
+					val.preço,
+				)
+				
 			} else {
-				fmt.Println("\033[34m[",ind,"]",val.nome,"\033[m")
-				fmt.Printf("->\033[92m\tpreço: R$%.2f\033[m\t\n",val.preço)
+				fmt.Printf(
+					"\033[34m[ %d ] %s \033[m\n",
+					ind, val.nome,
+					)
+				fmt.Printf(
+					"preço:\033[92m R$%.2f\033[m\n",
+					val.preço,
+					)
 			}
 			println()
 		}
 		
 		
 		if len(carrinho) > 0 {
-			fmt.Println("Meu carrinho:\n")
+			fmt.Println("Meu carrinho:")
 			for _,v := range carrinho{
-				fmt.Printf("%s\n",v.nome)
+				fmt.Printf("->\t%s\n",v.nome)
 			}
-				
+			println()
 		}
 		
 		
@@ -104,8 +162,7 @@ func main(){
 		
 		var exit int 
 		for {
-			fmt.Println("[ 1 ] sair")
-			fmt.Println("[ 2 ] continuar")
+			fmt.Println("[ 1 ] sair\n[ 2 ] continuar")
 			fmt.Print("oq deseja fazer: ")
 			err, _ := fmt.Scan(&exit)
 			if err == 1 {
